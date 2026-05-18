7f36308824d8b3c8c656d3f050d44c12ee2ac083
MarceloAM94 <amayamedinam@gmail.com>
2026-05-17 23:34:51 -0500

    feat(repository): Crear contrato de persistencia e implemetacion en memoria

--- src/main/java/gym/repository/UsuarioMemoryRepository.java
+++ src/main/java/gym/repository/UsuarioMemoryRepository.java
@@ -1,4 +1,40 @@
 package gym.repository;
+import gym.model.Usuario;
+import java.util.*;
 
-public class UsuarioMemoryRepository {
+public class UsuarioMemoryRepository implements UsuarioRepository{
+
+    private final Map<UUID, Usuario> datasource = new HashMap<>();
+
+    @Override
+    public void guardar(Usuario usuario) {
+        if (usuario == null || usuario.getId() == null){
+            throw new IllegalArgumentException("No se puede guardar un usuario nulo o sin ID legitimo.");
+        }
+        datasource.put(usuario.getId(), usuario);
+    }
+
+    @Override
+    public Optional<Usuario> buscarPorId(UUID id) {
+        return Optional.ofNullable(datasource.get(id));
+    }
+
+    @Override
+    public Optional<Usuario> buscarPorEmail(String email) {
+        if (email == null) return Optional.empty();
+
+        return datasource.values().stream()
+                .filter(usuario -> email.equalsIgnoreCase(usuario.getEmail()))
+                .findFirst();
+    }
+
+    @Override
+    public List<Usuario> listarTodos() {
+        return new ArrayList<>(datasource.values());
+    }
+
+    @Override
+    public void eliminar(UUID id) {
+        datasource.remove(id);
+    }
 }
--- src/main/java/gym/repository/UsuarioRepository.java
+++ src/main/java/gym/repository/UsuarioRepository.java
@@ -1,4 +1,14 @@
 package gym.repository;
+import gym.model.Usuario;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-public interface UsuarioRepository {
+public interface UsuarioRepository { // <-- Cambiado de class a interface
+
+    void guardar(Usuario usuario);
+    Optional<Usuario> buscarPorId(UUID id);
+    Optional<Usuario> buscarPorEmail(String email);
+    List<Usuario> listarTodos();
+    void eliminar(UUID id);
 }
